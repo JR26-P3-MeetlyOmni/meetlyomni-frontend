f975526eeff45ee8b1cc7f862d1c328a11904fa9
depeng zhang <zhangdepeng3@gmail.com>
2025-07-25 15:52:15 +1000

    chore:add relevant dependency

--- src/components/LandingPage/applicable-scenarios/ScenariosSection.stories.tsx
+++ src/components/LandingPage/applicable-scenarios/ScenariosSection.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/nextjs';
+import '@storybook/testing-library';
 
 import type { ScenariosSectionProps } from './interface';
 import ScenariosSection from './ScenariosSection';
