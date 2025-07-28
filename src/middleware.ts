b4759067627f5adb2e6257f78b9416f8b9e5d221
depeng zhang <zhangdepeng3@gmail.com>
2025-07-28 14:00:35 +1000

    feat:add messages and i18n related files

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,11 +5,8 @@ import { routing } from './i18n/routing';
 export default createMiddleware(routing);
 
 export const config = {
-<<<<<<< HEAD
   // Match all pathnames except for
   // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-=======
->>>>>>> a17400f (re-structure to app router structure & config i18n (#17))
   matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)',
 };
