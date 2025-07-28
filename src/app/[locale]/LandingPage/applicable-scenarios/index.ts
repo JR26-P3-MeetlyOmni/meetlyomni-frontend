880ae7977b6d07f27520e7e63c78f3f6d2befb45
depeng zhang <zhangdepeng3@gmail.com>
2025-07-28 14:48:27 +1000

    move the grid to local test i18n

---
+++ src/app/[locale]/LandingPage/applicable-scenarios/index.ts
@@ -0,0 +1,2 @@
+export { default as ScenariosSection } from './ScenarioSection';
+export { SAMPLE_SCENARIOS } from './data';
---
+++ src/app/[locale]/LandingPage/applicable-scenarios/interface.ts
@@ -0,0 +1,18 @@
+export interface ScenarioData {
+  id: string;
+  title: string;
+  descriptions: string[];
+  image: string;
+  imageAlt: string;
+}
+
+export interface ScenarioCardProps {
+  scenario: ScenarioData;
+  className?: string;
+}
+
+export interface ScenariosSectionProps {
+  scenarios: ScenarioData[];
+  title?: string;
+  className?: string;
+}
