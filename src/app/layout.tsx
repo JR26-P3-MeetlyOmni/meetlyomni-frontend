e81338dcf38771abb08fdd8aeb6549bd757de68c
depeng zhang <zhangdepeng3@gmail.com>
2025-08-04 18:49:59 +1000

    feat:add MUIProvider instead of clientLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,10 @@
-import ClientLayout from '@/components/ClientLayout';
+import { MUIProvider } from '@/components/Providers/MUIProvider';
 import { ReduxProvider } from '@/store/provider';
 
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import { Roboto } from 'next/font/google';
 
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 const roboto = Roboto({
   weight: ['400', '500', '700'],
@@ -35,11 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${roboto.variable} ${geistSans.variable} ${geistMono.variable}`}>
-        <ReduxProvider>
-          <ClientLayout>{children}</ClientLayout>
-        </ReduxProvider>
+    <html lang="en" className={roboto.variable}>
+      <body>
+        <MUIProvider>
+          <ReduxProvider>{children}</ReduxProvider>
+        </MUIProvider>
       </body>
     </html>
   );
