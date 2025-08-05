8ad0e7212a88f6e365b897bcfccb9d9f36f25233
hulin <piaohulin2007@gmail.com>
2025-08-06 00:46:31 +1000

    remove unused vars

--- src/app/[locale]/(main)/components/Footer/Footer.tsx
+++ src/app/[locale]/(main)/components/Footer/Footer.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { URL_CONFIG } from '@/constant/footer_external_links';
-import theme from '@/theme';
 
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
