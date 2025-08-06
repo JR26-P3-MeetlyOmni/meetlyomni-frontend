b4d158efb12b9896b40fc9f28e672adab567463d
Nicole L <nicole.chunyang.li@gmail.com>
2025-08-07 00:18:12 +1000

    update

--- src/components/Providers/MUIProvider.tsx
+++ src/components/Providers/MUIProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { theme } from '@/theme';
+import theme from '@/theme';
 
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
