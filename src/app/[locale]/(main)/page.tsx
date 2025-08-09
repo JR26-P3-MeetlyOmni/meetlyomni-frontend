cd75a130ddcc8db16bfab452664726c7f651f930
cliu61 <liuchangaliens@gmail.com>
2025-08-09 23:49:59 +1000

    Update to fix FaqAccordion display issue

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -1,28 +1,21 @@
 import { getTranslations } from 'next-intl/server';
 
-import { Box } from '@mui/material';
-
-import { ProductFeaturesData, ProjectFeaturesData } from '../../../constants/FeaturesData';
-import FeatureCardGrid from './components/FeatureCard/CardGrid/FeatureCardGrid';
-import ProductTitle from './components/FeatureCard/ProductTitle';
-import { ScenariosSection } from './components/ScenariosSection';
 import FaqAccordion from './components/FaqAccordion';
 
 export default async function LocalePage({ params }: { params: Promise<{ locale: string }> }) {
   const { locale } = await params;
   const t = await getTranslations('home');
+  const tLanding = await getTranslations('LandingPage');
 
   return (
-    <Box sx={{ textAlign: 'center', mt: 12 }}>
-      <h1>{t('title')}</h1>
-      <p>{t('underConstruction')}</p>
-      <p>{t('currentLocale', { locale })}</p>
-
-      <FeatureCardGrid data={ProjectFeaturesData} type="project" />
-      <ProductTitle />
-      <FeatureCardGrid data={ProductFeaturesData} type="product" />
-      <ScenariosSection />
+    <>
+      <div style={{ textAlign: 'center', marginTop: '100px' }}>
+        <h1>{t('title')}</h1>
+        <p>{t('underConstruction')}</p>
+        <p>{t('currentLocale', { locale })}</p>
+        <p>FAQ Title: {tLanding('faq.title')}</p>
+      </div>
       <FaqAccordion />
-    </Box>
+    </>
   );
 }
---
+++ src/theme/index.ts
@@ -0,0 +1,6 @@
+import { createTheme } from '@mui/material/styles';
+
+// Use MUI's default theme
+export const theme = createTheme();
+
+export default theme;
