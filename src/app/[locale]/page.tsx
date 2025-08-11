af5e980c76283f0f04019677d81dba0cf3247360
Nicole L <nicole.chunyang.li@gmail.com>
2025-08-11 16:48:16 +1000

    update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,3 @@
-import { getTranslations } from 'next-intl/server';
 import React from 'react';
 
 import { Box } from '@mui/material';
@@ -6,17 +5,9 @@ import { Box } from '@mui/material';
 import { FeatureCard } from './(main)/components/FeatureCard';
 import { ScenariosSection } from './(main)/components/ScenariosSection';
 
-export default async function LocalePage({ params }: { params: Promise<{ locale: string }> }) {
-  const { locale } = await params;
-  const t = await getTranslations('home');
-  const _tCommon = await getTranslations('common');
-
+export default async function LocalePage() {
   return (
-    <Box sx={{ textAlign: 'center', mt: 12 }}>
-      <h1>{t('title')}</h1>
-      <p>{t('underConstruction')}</p>
-      <p>{t('currentLocale', { locale })}</p>
-
+    <Box>
       <FeatureCard />
       <ScenariosSection />
     </Box>
