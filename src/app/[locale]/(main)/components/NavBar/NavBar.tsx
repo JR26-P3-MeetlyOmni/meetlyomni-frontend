4b36097a32120d058e8710621628566c7e6ba290
Nicole L <158861083+Nicole-CY@users.noreply.github.com>
2025-08-13 11:10:37 +1000

    add logo

--- src/app/[locale]/(main)/components/NavBar/NavBar.tsx
+++ src/app/[locale]/(main)/components/NavBar/NavBar.tsx
@@ -7,16 +7,17 @@ import { Box, Button, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { alpha } from '@mui/material/styles';
 
+import Logo from '@assets/images/navbar/nav_bar_logo.png';
+
 const StickyNavbarWrapper = styled(Box)(({ theme }) => ({
   position: 'sticky',
   top: 0,
   width: '100%',
   padding: theme.spacing(2, 4),
-  backgroundColor: theme.palette.background.default,
-  zIndex: theme.zIndex.appBar,
   display: 'flex',
   alignItems: 'center',
-  boxSizing: 'border-box',
+  backgroundColor: theme.palette.background.default,
+  zIndex: theme.zIndex.appBar,
   marginBottom: theme.spacing(10),
   transition: theme.transitions.create(['background-color', 'box-shadow'], {
     duration: theme.transitions.duration.short,
@@ -33,6 +34,18 @@ const LogoWrapper = styled(Box)(({ theme }) => ({
   cursor: 'pointer',
   gap: theme.spacing(1.5),
   marginLeft: theme.spacing(20),
+  '& img': {
+    width: theme.spacing(16),
+    height: theme.spacing(4.5),
+    objectFit: 'contain',
+    transition: theme.transitions.create('width'),
+  },
+  [theme.breakpoints.down('md')]: {
+    marginLeft: theme.spacing(2),
+    '& img': {
+      width: theme.spacing(14),
+    },
+  },
 }));
 
 const NavLinksWrapper = styled(Box)(({ theme }) => ({
@@ -41,6 +54,10 @@ const NavLinksWrapper = styled(Box)(({ theme }) => ({
   gap: theme.spacing(4),
   marginLeft: 'auto',
   marginRight: theme.spacing(7),
+  [theme.breakpoints.down('md')]: {
+    marginRight: theme.spacing(3),
+    marginLeft: theme.spacing(6),
+  },
 }));
 
 const NavLink = styled(Typography)(({ theme }) => ({
@@ -49,6 +66,7 @@ const NavLink = styled(Typography)(({ theme }) => ({
   fontWeight: theme.typography.fontWeightBold,
   color: theme.palette.text.primary,
   cursor: 'pointer',
+  whiteSpace: 'nowrap',
   '&:hover': {
     textDecoration: 'underline',
     textDecorationColor: theme.palette.text.primary,
@@ -62,6 +80,9 @@ const ButtonGroupWrapper = styled(Box)(({ theme }) => ({
   alignItems: 'center',
   gap: theme.spacing(1.25),
   marginRight: theme.spacing(20),
+  [theme.breakpoints.down('md')]: {
+    marginRight: theme.spacing(2),
+  },
 }));
 
 const CTAButton = styled(Button)(({ theme }) => ({
@@ -117,8 +138,7 @@ const NavBar: React.FC = () => {
     <StickyNavbarWrapper>
       {/* Logo */}
       <LogoWrapper>
-        <Image src="/logo.png" alt="Omni Logo" width={32} height={32} />
-        <Typography variant="h6">Omni</Typography>
+        <Image src={Logo} alt="Omni Logo" />
       </LogoWrapper>
 
       {/* Nav Links */}
