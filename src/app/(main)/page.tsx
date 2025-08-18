a5e091537dc1dfe2829583da7d131bfc505cf937
cliu61 <106297163+cliu61@users.noreply.github.com>
2025-08-18 16:30:43 +1000

    Revert "Update FaqAccordion section, based on previous code review." (#60)
    Hi team, I accidentally merged
    [feature/scrum-10-FAQ-Accordion-Chang](https://github.com/JR26-P3-MeetlyOmni/meetlyomni-frontend/tree/feature/scrum-10-FAQ-Accordion-Chang)
    into dev without "squash and merge" command in PR. Therefore the merge
    commit is lack of "verified" and my multiple personal commits are merged
    into dev as well.
    
    <img width="1590" height="894" alt="Image_20250817202221_58"
    src="https://github.com/user-attachments/assets/fd3698ed-19ae-4bf5-a794-3708b3eba725"
    />
    
    
    So here I ask to revert JR26-P3-MeetlyOmni/meetlyomni-frontend#20.
    
    @joeqiwx @hulinpiao @Nicole-CY Since you have approved #20 before, could
    you take some time to approve current revert PR again? Thank you for
    your understanding!

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,5 @@
 import { Box } from '@mui/material';
 
-import FaqAccordion from './components/FaqAccordion';
 import { FeatureCard } from './components/FeatureCard';
 import { ScenariosSection } from './components/ScenariosSection';
 
@@ -9,7 +8,6 @@ export default async function localPage() {
     <Box>
       <FeatureCard />
       <ScenariosSection />
-      <FaqAccordion />
     </Box>
   );
 }
--- src/components/QRCode/QRCode.test.tsx
+++ src/components/QRCode/QRCode.test.tsx
@@ -7,7 +7,7 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { act, render, screen, waitFor } from '@testing-library/react';
 
-import QRCodeComponent from './QRCode';
+import QRCodeComponent from './qrcode';
 
 vi.mock('qrcode', () => ({
   default: {
