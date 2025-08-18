21e861523f1a6e006fa724d2ccf7f17f3802501f
cliu61 <liuchangaliens@gmail.com>
2025-08-18 18:03:58 +1000

    Keep trying to solve more conflicts

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,14 +1,10 @@
 import { Box } from '@mui/material';
 
 import FaqAccordion from './components/FaqAccordion';
-import { FeatureCard } from './components/FeatureCard';
-import { ScenariosSection } from './components/ScenariosSection';
 
 export default async function localPage() {
   return (
     <Box>
-      <FeatureCard />
-      <ScenariosSection />
       <FaqAccordion />
     </Box>
   );
