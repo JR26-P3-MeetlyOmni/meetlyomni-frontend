6ba054b1ed0a5d21e8fa4ec2f9cc5f30c8253c1c
depeng zhang <zhangdepeng3@gmail.com>
2025-08-22 20:16:48 +1000

    feat:add page label

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,9 @@
+import EmailStep from './pages/EmailStep';
+
+export default function SignupPage() {
+  return (
+    <div>
+      <EmailStep />
+    </div>
+  );
+}
---
+++ src/app/(auth)/signup/pages/EmailStep.tsx
@@ -0,0 +1,13 @@
+import { PageTitle } from '../components/PageLabel';
+
+const EmailStep = () => {
+  return (
+    <div>
+      <PageTitle
+        title="Please Enter Your Email Address"
+        subtitle="This email address will be used as your primary account"
+      />
+    </div>
+  );
+};
+export default EmailStep;
