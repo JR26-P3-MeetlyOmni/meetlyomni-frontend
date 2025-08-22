d0cb81f80e88712118b9c602794f0aa391ca9b50
depeng zhang <zhangdepeng3@gmail.com>
2025-08-22 20:48:49 +1000

    feat:add smileyCard

--- src/app/(auth)/signup/layout.tsx
+++ src/app/(auth)/signup/layout.tsx
@@ -1,10 +1,12 @@
 import { SigninButton } from './components/SigninButton';
+import { SmileyCard } from './components/SmileyCard';
 import { TopLeftLogo } from './components/TopLeftLogo';
 
 export default function SignupLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <TopLeftLogo imgSrc="/assets/images/Signup/top-left-logo.png" imgAlt="logo" />
+      <SmileyCard imgSrc="/assets/images/Signup/smiley-face.png" imgAlt="smiley face card" />
       <SigninButton />
       {children}
     </div>
