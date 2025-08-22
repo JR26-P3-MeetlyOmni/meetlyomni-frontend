758e423abbcfbef2ff59884eb11734e9498a1b8f
depeng zhang <zhangdepeng3@gmail.com>
2025-08-22 19:50:00 +1000

    feat:add signin button

--- src/app/(auth)/signup/layout.tsx
+++ src/app/(auth)/signup/layout.tsx
@@ -1,9 +1,11 @@
+import { SigninButton } from './components/SigninButton';
 import { TopLeftLogo } from './components/TopLeftLogo';
 
 export default function SignupLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <TopLeftLogo imgSrc="/assets/images/Signup/top-left-logo.png" imgAlt="logo" />
+      <SigninButton />
       {children}
     </div>
   );
