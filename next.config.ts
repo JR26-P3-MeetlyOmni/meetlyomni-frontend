1400784ce8bef93c033b2229bc91c2e9c9a2a213
cliu61 <liuchangaliens@gmail.com>
2025-10-08 18:04:36 +1100

    Update new config for images loading

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ const nextConfig = {
   // Enable standalone output for Docker optimization
   output: 'standalone',
   // Your existing Next.js config
+
+  // new config for images
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_CDN_HOSTNAME,
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
