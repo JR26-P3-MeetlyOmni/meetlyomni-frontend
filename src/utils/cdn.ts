4202639c84da00a77c7a88786ebd5bdf1ca61112
cliu61 <liuchangaliens@gmail.com>
2025-10-08 19:40:10 +1100

    remove unnecessary comment

--- src/utils/cdn.ts
+++ src/utils/cdn.ts
@@ -2,6 +2,6 @@ export const CDN_URL = process.env.NEXT_PUBLIC_CDN_URL || '';
 
 export const getAssetUrl = (path: string) => {
   // path: "StaticFiles/assets/images/.../XXX.png"
-  if (!CDN_URL) return `/${path}`; // local development fallback
+  if (!CDN_URL) return `/${path}`;
   return `${CDN_URL}/${path}`;
 };
